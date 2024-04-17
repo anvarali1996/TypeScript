fde98274513efa623955b6f9c0c8e21527500810
Anvarjon <anvarjonsheraliev@gmail.com>
2024-04-17 15:11:02 +0300

    Functions

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-var header2 = {
-    id: 1,
-    name: "Sattor",
-    hona: "X-7",
-    students: 123,
+// function getData(a:number, b: number) {
+//     return a ** b;
+// }
+function getData(prop) {
+    prop();
+}
+var fun = function () {
+    console.log("fun is working");
+    return 123;
 };
-// let header1: sign = {
-//     id: 1,
-//     name: "Gani",
-//     schedule: "Algebry",
-//     students: 123,
-//     moderaor: "Alex",
-// };
-var getData = function (i) {
-    // const getData = (i: keyof sign) => {
-    return header2[i];
-};
-console.log(getData("hona"));
+fun.title = "Anvarjon";
+console.log(getData(fun));
--- index.ts
+++ index.ts
@@ -1,29 +1,21 @@
-interface Sign {
-    // [index: string]: string | number | [];
-    id: number;
-    name: string;
-    students: number;
-    hona: string;
+// function getData(a:number, b: number) {
+//     return a ** b;
+// }
+
+// console.log(getData(4, 2));
+
+type Prop = {
+    title: string;
+    (): number;
 }
 
-let header2: Sign = {
-    id: 1,
-    name: "Sattor",
-    hona: "X-7",
-    students: 123,
-};
+function getData(prop:Prop) {
+    prop()
+}
 
-// let header1: sign = {
-//     id: 1,
-//     name: "Gani",
-//     schedule: "Algebry",
-//     students: 123,
-//     moderaor: "Alex",
-// };
-
-const getData = (i: keyof typeof header2) => {
-// const getData = (i: keyof sign) => {
-    return header2[i];
-};
-
-console.log(getData("hona"));
+const fun = () => {
+    console.log("fun is working");
+    return 123;
+}
+fun.title = "Anvarjon";
+console.log(getData(fun));
